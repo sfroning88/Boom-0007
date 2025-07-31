85bdd9c0b656640d8a371a9836b0d07fbb066d34
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-31 17:09:00 -0500

    Update classify.py
    Customer and vendor functionality

--- functions/classify.py
+++ functions/classify.py
@@ -1,9 +1,19 @@
 def classify_file(filename):
     import re
-    from support.filetypes import journal_patterns
     
+    from support.filetypes import journal_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in journal_patterns):
         print("\nFound filetype: journal\n")
         return "journal"
+
+    from support.filetypes import customer_patterns
+    if any(re.search(pattern, filename, re.IGNORECASE) for pattern in customer_patterns):
+        print("\nFound filetype: customer\n")
+        return "customer"
+
+    from support.filetypes import vendor_patterns
+    if any(re.search(pattern, filename, re.IGNORECASE) for pattern in vendor_patterns):
+        print("\nFound filetype: vendor\n")
+        return "vendor"
 
     return "misc"

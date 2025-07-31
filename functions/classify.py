7c00182d489f5999dfc9e4b41427c01b69eb1c76
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-31 11:57:16 -0500

    Update classify.py
    Fixed re logic

--- functions/classify.py
+++ functions/classify.py
@@ -2,9 +2,8 @@ def classify_file(filename):
     import re
     from support.filetypes import journal_patterns
     
-    for pattern in journal_patterns:
-        if any(re.search(pattern, filename, re.IGNORECASE)):
-            print("\nFound filetype: journal\n")
-            return "journal"
+    if any(re.search(pattern, filename, re.IGNORECASE) for pattern in journal_patterns):
+        print("\nFound filetype: journal\n")
+        return "journal"
 
     return "misc"

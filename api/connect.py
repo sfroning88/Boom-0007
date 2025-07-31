52dc920363211a7c898772576aa5df641f58999b
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-31 08:25:08 -0500

    Update connect.py
    QBO Dev capabilities

--- api/connect.py
+++ api/connect.py
@@ -1,2 +1,6 @@
-def connect_qbo(qbo_token):
+def connect_qbo(qbo_token, qbo_account):
+    
+    qbo_url = "https://sandbox-quickbooks.api.intuit.com/v3"
+    qbo_app = "940022df-bc3f-4ead-8172-7557c7ad913d"
+    
     pass

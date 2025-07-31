f08752b804c964722a83bdba4a2e1f3fc93e0cf8
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-31 11:51:56 -0500

    Update classify.py
    Basic classify statement

--- functions/classify.py
+++ functions/classify.py
@@ -1,3 +1,10 @@
 def classify_file(filename):
+    import re
+    from support.filetypes import journal_patterns
+    
+    for pattern in journal_patterns:
+        if any(re.search(pattern, filename, re.IGNORECASE)):
+            print("\nFound filetype: journal\n")
+            return "journal"
 
-    pass
+    return "misc"

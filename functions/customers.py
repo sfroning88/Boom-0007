3140a5a09847ddcd0c60f502c5f3e639b6b36fd9
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-01 09:58:02 -0500

    Update customers.py
    Post customers concurrently

--- functions/customers.py
+++ functions/customers.py
@@ -23,12 +23,12 @@ def extract_customers(file, exte):
         
         try:
             current_customer = {
-                'Customer': str(row.iloc[2]) if pd.notna(row.iloc[2]) else None,
-                'Bill To': str(row.iloc[4]) if pd.notna(row.iloc[4]) else None,
-                'Primary Contact': str(row.iloc[6]) if pd.notna(row.iloc[6]) else None,
-                'Main Phone': str(row.iloc[8]) if pd.notna(row.iloc[8]) else None,
-                'Fax': str(row.iloc[10]) if pd.notna(row.iloc[10]) else None,
-                'Balance Total': round(float(row.iloc[12]), 2) if pd.notna(row.iloc[12]) else None
+                'Customer': str(row.iloc[2]) if pd.notna(row.iloc[2]) else '',
+                'Bill To': str(row.iloc[4]) if pd.notna(row.iloc[4]) else '',
+                'Primary Contact': str(row.iloc[6]) if pd.notna(row.iloc[6]) else '',
+                'Main Phone': str(row.iloc[8]) if pd.notna(row.iloc[8]) else '',
+                'Fax': str(row.iloc[10]) if pd.notna(row.iloc[10]) else '',
+                'Balance Total': round(float(row.iloc[12]), 2) if pd.notna(row.iloc[12]) else ''
             }
 
             extracted[customer_counter] = current_customer

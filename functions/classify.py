29d934380e9857246bfe868b5cab46855807e75d
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-02 17:02:06 -0500

    Update classify.py
    MVP version

--- functions/classify.py
+++ functions/classify.py
@@ -1,19 +1,25 @@
 def classify_file(filename):
+    print("##############################_CLASS_BEGIN_##############################")
+    
     import re
     
     from support.filetypes import journal_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in journal_patterns):
-        print("\nFound filetype: journal\n")
+        print("Found filetype: journal")
+        print("##############################_CLASS_END_##############################")
         return "journal"
 
     from support.filetypes import customer_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in customer_patterns):
-        print("\nFound filetype: customer\n")
+        print("Found filetype: customer")
+        print("##############################_CLASS_END_##############################")
         return "customer"
 
     from support.filetypes import vendor_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in vendor_patterns):
-        print("\nFound filetype: vendor\n")
+        print("Found filetype: vendor")
+        print("##############################_CLASS_END_##############################")
         return "vendor"
 
+    print("##############################_CLASS_END_##############################")
     return "misc"

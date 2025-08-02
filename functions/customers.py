4abbf53fd0ef97d6e51018f4f2ff6330552bd096
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-02 17:02:26 -0500

    Update customers.py
    MVP version

--- functions/customers.py
+++ functions/customers.py
@@ -1,4 +1,6 @@
 def extract_customers(file, exte):
+    print("##############################_EXTRC_BEGIN_##############################")
+
     # Generic dictionary to store customers
     extracted = {}
 
@@ -9,26 +11,28 @@ def extract_customers(file, exte):
     elif exte == 'xlsx' or exte == 'xls':
         df = pd.read_excel(file)
     else:
+        print("##############################_EXTRC_END_##############################")
         return {}
     
     num_rows, num_cols = df.shape
 
-    print(f"\n{num_rows} Rows, {num_cols} Cols were ingested\n")
+    print(f"{num_rows} Rows, {num_cols} Cols were ingested")
 
     # Extract customer
     customer_counter = 0
 
+    from functions.stripping import strip_nonabc
     for i in range(len(df)):
         row = df.iloc[i]
         
         try:
             current_customer = {
-                'Customer': str(row.iloc[2]) if pd.notna(row.iloc[2]) else '',
-                'Bill To': str(row.iloc[4]) if pd.notna(row.iloc[4]) else '',
-                'Primary Contact': str(row.iloc[6]) if pd.notna(row.iloc[6]) else '',
-                'Main Phone': str(row.iloc[8]) if pd.notna(row.iloc[8]) else '',
-                'Fax': str(row.iloc[10]) if pd.notna(row.iloc[10]) else '',
-                'Balance Total': round(float(row.iloc[12]), 2) if pd.notna(row.iloc[12]) else ''
+                'Customer': strip_nonabc(row.iloc[2]) if pd.notna(row.iloc[2]) else None,
+                'Bill To': str(row.iloc[4]) if pd.notna(row.iloc[4]) else None,
+                'Primary Contact': str(row.iloc[6]) if pd.notna(row.iloc[6]) else None,
+                'Main Phone': str(row.iloc[8]) if pd.notna(row.iloc[8]) else None,
+                'Fax': str(row.iloc[10]) if pd.notna(row.iloc[10]) else None,
+                'Balance Total': round(float(row.iloc[12]), 2) if pd.notna(row.iloc[12]) else None
             }
 
             extracted[customer_counter] = current_customer
@@ -44,4 +48,5 @@ def extract_customers(file, exte):
     for key, value in first_customer.items():
         print(f"  {key}: {value}")
 
+    print("##############################_EXTRC_END_##############################")
     return extracted

c0267b37c4c7a0d9662ac3995a62d35b5d4d4a14
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-05 11:32:36 -0500

    Update extension.py
    100% post rate

--- functions/extension.py
+++ functions/extension.py
@@ -3,7 +3,3 @@ ALLOWED_EXTENSIONS = ['csv', 'xls', 'xlsx']
 def retrieve_extension(filename):
     exte = '.' in filename and filename.rsplit('.', 1)[1].lower()
     return exte.strip().lower()
-
-def strip_timestamp(date):
-    import re
-    return re.sub(r'\s+\d{1,2}:\d{2}(:\d{2})?$', '', date)

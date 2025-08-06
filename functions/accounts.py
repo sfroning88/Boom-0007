291fafd0b3a64321e8ac06ced8f4cb4074bfeae0
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-06 14:08:07 -0500

    Create accounts.py
    Map expenses to CoA

---
+++ functions/accounts.py
@@ -0,0 +1,40 @@
+def extract_accounts(file, exte):
+    print("##############################_EXTRA_BEGIN_##############################")
+
+    # Generic dictionary to store accounts
+    extracted = {}
+
+    # Convert file into pandas dataframe
+    import pandas as pd
+    if exte == 'csv':
+        df = pd.read_csv(file)
+    elif exte == 'xlsx' or exte == 'xls':
+        df = pd.read_excel(file)
+    else:
+        print("##############################_EXTRA_END_##############################")
+        return {}
+
+    # Extract vendors
+    account_counter = 0
+
+    from functions.stripping import strip_nonabc
+    for i in range(len(df)):
+        row = df.iloc[i]
+        
+        current_account = {
+            'Old': strip_nonabc(row.iloc[0]) if pd.notna(row.iloc[0]) else None,
+            'Num': int(row.iloc[1]) if pd.notna(row.iloc[1]) else None,
+            'Name': str(row.iloc[2]) if pd.notna(row.iloc[2]) else None,
+        }
+
+        current_account['Full'] = str(current_account['Num']) + " " + current_account['Name'] if 'Num' is not None and 'Name' is not None else None
+
+        extracted[account_counter] = current_account
+        account_counter += 1
+
+    first_account_key = list(extracted.keys())[0]
+    first_account = extracted[first_account_key]
+    print(f"CHECKPOINT: {first_account_key} accesses {first_account['Full']}")
+
+    print("##############################_EXTRA_END_##############################")
+    return extracted

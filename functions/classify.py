87e0768124aeef320f77f90bbfbceec7064fd400
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-06 14:08:28 -0500

    Update classify.py
    Map expenses to CoA

--- functions/classify.py
+++ functions/classify.py
@@ -5,21 +5,28 @@ def classify_file(filename):
     
     from support.filetypes import journal_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in journal_patterns):
-        print("Found filetype: journal")
+        print("CHECKPOINT: Found filetype: journal")
         print("##############################_CLASS_END_##############################")
         return "journal"
 
     from support.filetypes import customer_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in customer_patterns):
-        print("Found filetype: customer")
+        print("CHECKPOINT: Found filetype: customer")
         print("##############################_CLASS_END_##############################")
         return "customer"
 
     from support.filetypes import vendor_patterns
     if any(re.search(pattern, filename, re.IGNORECASE) for pattern in vendor_patterns):
-        print("Found filetype: vendor")
+        print("CHECKPOINT: Found filetype: vendor")
         print("##############################_CLASS_END_##############################")
         return "vendor"
 
+    from support.filetypes import account_patterns
+    if any(re.search(pattern, filename, re.IGNORECASE) for pattern in account_patterns):
+        print("CHECKPOINT: Found filetype: account")
+        print("##############################_CLASS_END_##############################")
+        return "account"
+
+    print("WARNING: Failed to classify the file, please rename")
     print("##############################_CLASS_END_##############################")
     return "misc"

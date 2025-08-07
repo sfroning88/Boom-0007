71fa9f0a81c5f4d69bf38a5ccfabde686b549a77
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-08-07 13:42:49 -0500

    Update resolve.py
    Simplified functions

--- api/resolve.py
+++ api/resolve.py
@@ -1,154 +1,117 @@
-def resolve_customers(invoices_extracted):
+def resolve_objects(extracted=None, object_mode=None):
+    if extracted is None or object_mode is None:
+        print("ERROR: No object or mode was passed into resolve function")
+        return None
+
+    if object_mode not in ["Customer", "Vendor"]:
+        print("ERROR: Incorrect enum mode passed into resolve function")
+        return None
+
     import concurrent.futures
     from tqdm import tqdm
 
     from api.retrieve import get_database
-    customers_pre = get_database(query_mode="Customer")
+    pre_object = get_database(query_mode=object_mode)
 
-    if customers_pre is None or 'Customer' not in list(customers_pre.keys()):
-        print("WARNING: Please upload Customers to QBO before posting invoices")
+    if pre_object is None or object_mode not in list(pre_object.keys()):
+        print(f"WARNING: Please upload {object_mode}s to QBO before posting invoices")
         return None
 
-    customers_post = customers_pre['Customer']
+    post_object = pre_object[object_mode]
 
-    # pull all existing customers
-    customers_existing = []
-    for customer in customers_post:
-        customers_existing.append(customer['DisplayName'])
-    if len(customers_existing) == 0:
-        return invoices_extracted
+    # pull all existing objects
+    objects_existing = []
+    for single in post_object:
+        objects_existing.append(single['DisplayName'])
+    if len(objects_existing) == 0:
+        return extracted
 
-    print(f"CHECKPOINT: Found {len(customers_existing)} existing Customers")
+    print(f"CHECKPOINT: Found {len(objects_existing)} existing {object_mode}")
 
-    customers_new = []
-    for customer_object in list(invoices_extracted.values()):
-        customers_new.append(customer_object['Name'])
-    customers_new = list(set(customers_new))
-    customers_new.sort()
+    objects_new = []
+    for single in list(extracted.values()):
+        objects_new.append(single['Name'])
+    objects_new = list(set(objects_new))
+    objects_new.sort()
 
     # remove any duplicate customers
-    for invoice_name in customers_new:
-        if invoice_name in customers_existing:
-            customers_new.remove(invoice_name)
+    for new_name in objects_new:
+        if new_name in objects_existing:
+            objects_new.remove(new_name)
 
-    customers_added = []
-    for invoice_name in customers_new:
-        # Create dummy customer object and add to database
-        dummy_customer = {
-            "Customer": invoice_name,
-            "Primary Contact": "",
-            "Main Phone": "",
-            "Bill To": "",
-            "Balance Total": 0.0
-        }
+    objects_added = []
+    for new_name in objects_new:
 
-        customers_added.append(dummy_customer)
+        if object_mode == "Customer":
+            # Create dummy customer object and add to database
+            dummy_customer = {
+                "Customer": new_name,
+                "Primary Contact": "",
+                "Main Phone": "",
+                "Bill To": "",
+                "Balance Total": 0.0
+            }
+            objects_added.append(dummy_customer)
 
-    print(f"CHECKPOINT: Found {len(customers_added)} new Customers to add")
+        elif object_mode == "Vendor":
+            dummy_vendor = {
+                "Vendor": new_name,
+                "Account #": "",
+                "Primary Contact": "",
+                "Main Phone": "",
+                "Bill From": "",
+                "Balance Total": 0.0
+            }
+            objects_added.append(dummy_vendor)
 
-    # Concurrently post all customers from customers
-    from api.customers import customer_threadsafe
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        list(tqdm(executor.map(customer_threadsafe, customers_added), total=len(customers_added)))
+    print(f"CHECKPOINT: Found {len(objects_added)} new {object_mode}s to add")
 
-    return invoices_extracted
+    # Concurrently post all objects
+    if object_mode == "Customer":
+        from api.customers import customer_threadsafe
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            list(tqdm(executor.map(customer_threadsafe, objects_added), total=len(objects_added)))
 
-def resolve_cust_ids(invoices_extracted):
-    from api.retrieve import get_database
-    customers_pre = get_database(query_mode="Customer")
-    customers_post = customers_pre['Customer']
+    elif object_mode == "Vendor":
+        from api.vendors import vendor_threadsafe
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            list(tqdm(executor.map(customer_threadsafe, objects_added), total=len(objects_added)))
 
-    # pull QBO id from customers
-    ids_mapping = {}
-    for customer in customers_post:
-        ids_mapping[customer['DisplayName']] = customer['Id']
-    if len(ids_mapping.values()) != len(ids_mapping.keys()):
-        print("ERROR: Customers does not equal ids, some other error occured")
-        return invoices_extracted
-    if len(list(ids_mapping.values())) == 0:
-        return invoices_extracted
+    return extracted
 
-    print(f"CHECKPOINT: Found {len(list(ids_mapping.values()))} existing Ids")
-
-    # assign each invoice the corresponding id
-    for invoice_object in list(invoices_extracted.values()):
-        if invoice_object['Name'] in list(ids_mapping.keys()):
-            invoice_object['Id'] = ids_mapping[invoice_object['Name']]
-
-    return invoices_extracted
-
-def resolve_vendors(bills_extracted):
-    import concurrent.futures
-    from tqdm import tqdm
-
-    from api.retrieve import get_database
-    vendors_pre = get_database(query_mode="Vendor")
-    if vendors_pre is None or 'Vendor' not in list(vendors_pre.keys()):
-        print("WARNING: Please upload Vendors to QBO before posting bills")
+def resolve_ids(extracted=None, object_mode=None):
+    if extracted is None or object_mode is None:
+        print("ERROR: No object or mode was passed into resolve function")
         return None
-    
-    vendors_post = vendors_pre['Vendor']
 
-    vendors_existing = []
-    for vendor in vendors_post:
-        vendors_existing.append(vendor['DisplayName'])
-    if len(vendors_existing) == 0:
-        return bills_extracted
+    if object_mode not in ["Customer", "Vendor"]:
+        print("ERROR: Incorrect enum mode passed into resolve function")
+        return None
 
-    print(f"CHECKPOINT: Found {len(vendors_existing)} existing Vendors")
-
-    vendors_new = []
-    for bill_object in list(bills_extracted.values()):
-        vendors_new.append(bill_object['Name'])
-    vendors_new = list(set(vendors_new))
-
-    # remove any duplicate vendors
-    for bill_name in vendors_new:
-        if bill_name in vendors_existing:
-            vendors_new.remove(bill_name)
-
-    vendors_added = []
-    for bill_name in vendors_new:
-        # Create dummy vendor object and add to database
-        dummy_vendor = {
-            "Vendor": bill_name,
-            "Account #": "",
-            "Primary Contact": "",
-            "Main Phone": "",
-            "Bill From": "",
-            "Balance Total": 0.0
-        }
-        vendors_added.append(dummy_vendor)
-
-    print(f"CHECKPOINT: Found {len(vendors_added)} new Vendors to add")
-
-    # Concurrently post all vendors from vendors
-    from api.vendors import vendor_threadsafe
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        list(tqdm(executor.map(vendor_threadsafe, vendors_added), total=len(vendors_added)))
-
-    return bills_extracted
-
-def resolve_vend_ids(bills_extracted):
     from api.retrieve import get_database
-    vendors_pre = get_database(query_mode="Vendor")
-    vendors_post = vendors_pre['Vendor']
+    pre_object = get_database(query_mode=object_mode)
 
-    # pull QBO id from customers
+    if pre_object is None or object_mode not in list(pre_object.keys()):
+        print(f"WARNING: Please upload {object_mode}s to QBO before posting invoices")
+        return None
+
+    post_object = pre_object[object_mode]
+
+    # pull QBO id from objects
     ids_mapping = {}
-    for vendor in vendors_post:
-        ids_mapping[vendor['DisplayName']] = vendor['Id']
+    for single in post_object:
+        ids_mapping[single['DisplayName']] = single['Id']
     if len(ids_mapping.values()) != len(ids_mapping.keys()):
-        print("ERROR: Vendors does not equal ids, some other error occured")
-        return bills_extracted
+        print(f"ERROR: {object_mode}s does not equal ids, some other error occured")
+        return extracted
     if len(list(ids_mapping.values())) == 0:
-        return bills_extracted
+        return extracted
 
-    print(f"CHECKPOINT: Found {len(list(ids_mapping.values()))} existing Ids")
+    print(f"CHECKPOINT: Found {len(list(ids_mapping.values()))} existing {object_mode} Ids")
 
-    # assign each invoice the corresponding id
-    for bill_object in list(bills_extracted.values()):
-        if bill_object['Name'] in list(ids_mapping.keys()):
-            bill_object['Id'] = ids_mapping[bill_object['Name']]
+    # assign each item the corresponding id
+    for item_object in list(extracted.values()):
+        if item_object['Name'] in list(ids_mapping.keys()):
+            item_object['Id'] = ids_mapping[item_object['Name']]
 
-    return bills_extracted
+    return extracted
